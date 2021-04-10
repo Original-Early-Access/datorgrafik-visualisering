858c18b1d4e8a58fad3e310b4686f2504337f090
MrDetroj <filip.47@hotmail.com>
2021-04-10 14:01:34 +0200

    CSV handler created

---
+++ Assets/CSVHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Assets
+{
+    class CSVHandler
+    {
+        public List<string[]> LoadCSV(string filename)
+        {
+            List<string[]> data = new List<string[]>();
+            if (File.Exists(filename))
+            {
+                using (StreamReader reader = new StreamReader(filename))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                        data.Add(CSVParser.Split(line));
+                    }
+                }
+            }
+            return data;
+        }
+    }
+}
